193b91aa00e1d53393a1e7ae776a81b92298c1df
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2015-09-04 13:08:13 -0400

    modified isotop code to execute after all images have loaded

--- assets/js/isotope.js
+++ assets/js/isotope.js
@@ -1,23 +1,15 @@
 jQuery(function ($) {
-
+	
 	var $container = $('#container');
 	// initialize Isotope
-	$container.isotope({
+	$(window).load(function(){
+		$container.isotope({
 		itemSelector : '.photo',
 		layoutMode: 'masonry',
 		// options
 		resizable: false,
 		// set columnWidth to a percentage of container width
 		masonry: { columnWidth: $container.width() / 4 }
+		});
 	});
-
-	
 });
-
-// update columnWidth on window resize
-	// $(window).smartresize(function(){
-	// 	$container.isotope({
-	// 		// update columnWidth to a percentage of container width
-	// 		masonry: { columnWidth: $container.width() / 4 }
-	// 	});
-	// });
